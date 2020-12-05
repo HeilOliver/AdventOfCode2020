e78ca44e315a77032eb81cc26200fc7f0344cbd6
OliverHeil <oliver.heil@jentschmann.ch>
2020-12-05 03:12:40 +0100

    Add Day 3 Challenge

---
+++ AdventSolver/Solver/DayThree/DayThreeSolver.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AdventSolver.Util;
+
+namespace AdventSolver.Solver.DayThree
+{
+    [AdventSolverAttribute(3)]
+    public class DayThreeSolver : IAdventSolver
+    {
+        private static IEnumerable<string> CreateExpenses()
+        {
+            var lines = File.ReadAllLines($"Data\\Day3.txt");
+            return lines;
+        }
+
+        public void Solve()
+        {
+            var expenses = CreateExpenses()
+                .ToArray();
+
+            int hitTrees = CalculateHitTrees(expenses, 3,1);
+            Console.WriteLine($"{hitTrees} Trees are hit when you go 3 right and 1 down");
+
+            long hitTreesMultiply = CalculateHitTreeSum(expenses);
+            Console.WriteLine($"{hitTreesMultiply} multiplied Trees");
+        }
+
+        private static long CalculateHitTreeSum(IReadOnlyList<string> map)
+        {
+            var values = new long[]
+            {
+                CalculateHitTrees(map, 1, 1),
+                CalculateHitTrees(map, 3, 1),
+                CalculateHitTrees(map, 5, 1),
+                CalculateHitTrees(map, 7, 1),
+                CalculateHitTrees(map, 1, 2),
+            };
+
+            return values.Multiply();
+        }
+
+        private static int CalculateHitTrees(IReadOnlyList<string> map, int right, int down)
+        {
+            int xPos = 0;
+            int treesHit = 0;
+            int mapLength = map[0].Length;
+
+            for (int i = 0; i < map.Count; i += down)
+            {
+                char pos = map[i][xPos % mapLength];
+                xPos += right;
+                if (pos == '#')
+                    treesHit++;
+            }
+
+            return treesHit;
+        }
+    }
+}

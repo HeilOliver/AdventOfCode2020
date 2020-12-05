8ff7d33aa624dda48c9b8ce51011bf25ee59908b
OliverHeil <oliver.heil@jentschmann.ch>
2020-12-05 03:12:25 +0100

    Add Day 2 Challenge

---
+++ AdventSolver/Solver/DayTwo/DayTwoSolver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventSolver.Solver.DayTwo
+{
+    [AdventSolverAttribute(2)]
+    public class DayTwoSolver : IAdventSolver
+    {
+        private static IEnumerable<Password> CreateExpenses()
+        {
+            var lines = File.ReadAllLines($"Data\\Day2.txt");
+            foreach (string line in lines)
+            {
+                yield return new Password(line);
+            }
+        }
+
+        public void Solve()
+        {
+            var passwords = CreateExpenses().ToList();
+
+            int validPasswords = passwords.Count(password => password.IsForRentalPlaceValid());
+            Console.WriteLine($"{validPasswords} are valid passwords for the sled rental place");
+
+            validPasswords = passwords.Count(password => password.IsForCorporateValid());
+            Console.WriteLine($"{validPasswords} are valid passwords for the Official Toboggan Corporate");
+        }
+    }
+}
---
+++ AdventSolver/Solver/DayTwo/Password.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+
+namespace AdventSolver.Solver.DayTwo
+{
+    public class Password
+    {
+        private readonly string password;
+        private readonly int min;
+        private readonly int max;
+        private readonly char toMatch;
+
+        public Password(string value)
+        {
+            var values = value.Split(": ");
+            password = values[1];
+            toMatch = values[0][values[0].Length -1];
+            var boundValues = values[0]
+                .Replace($" {toMatch}", "")
+                .Split("-");
+
+            min = int.Parse(boundValues[0]);
+            max = int.Parse(boundValues[1]);
+        }
+
+        public bool IsForRentalPlaceValid()
+        {
+            int count = password.Count(t => t == toMatch);
+            return count >= min && count <= max;
+        }
+
+        public bool IsForCorporateValid()
+        {
+            return password[min - 1] == toMatch ^ password[max - 1] == toMatch;
+        }
+    }
+}

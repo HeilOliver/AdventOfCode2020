fbe5f48a4ddc6b1c622fb080b8e7374cc652ab48
OliverHeil <oliver.heil@jentschmann.ch>
2020-12-05 03:12:47 +0100

    Add Day 4 Challenge

---
+++ AdventSolver/Solver/DayFour/DayFourSolver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventSolver.Solver.DayFour
+{
+    [AdventSolverAttribute(4)]
+    public class DayFourSolver : IAdventSolver
+    {
+        private IEnumerable<Passport> CreatePassports()
+        {
+            var lines = File.ReadAllLines($"Data\\Day4.txt");
+
+            StringBuilder currentBatch = new StringBuilder();
+
+            foreach (string line in lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    yield return new Passport(currentBatch.ToString());
+                    currentBatch = new StringBuilder();
+                    continue;
+                }
+                currentBatch.Append($" {line}");
+            }
+
+            yield return new Passport(currentBatch.ToString());
+        }
+
+        public void Solve()
+        {
+            var passports = CreatePassports()
+                .ToList();
+
+            int consistPassports = passports
+                .Count(passport => passport.IsConsistent());
+
+            Console.WriteLine($"{consistPassports} are consistent Passwords");
+
+            int validPassports = passports
+                .Count(passport => passport.IsValid());
+
+            Console.WriteLine($"{validPassports} are valid Passwords");
+        }
+    }
+}
---
+++ AdventSolver/Solver/DayFour/Passport.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using AdventSolver.Util;
+
+namespace AdventSolver.Solver.DayFour
+{
+    public class Passport
+    {
+        private readonly Dictionary<string, string> entries = new Dictionary<string, string>();
+
+        public Passport(string serializedPassport)
+        {
+            var kvValues = serializedPassport.Split(" ");
+            kvValues
+                .Where(val => !string.IsNullOrEmpty(val))
+                .Select(e => e.Split(":"))
+                .ToList()
+                .ForEach(e => entries.TryAdd(e[0], e[1]));
+        }
+
+        public bool IsConsistent()
+        {
+            return entries.ContainsKeys(new[]
+            {
+                "byr",
+                "iyr",
+                "eyr",
+                "hgt",
+                "hcl",
+                "ecl",
+                "pid"
+            });
+        }
+
+        public bool IsValid()
+        {
+            if (!IsConsistent())
+                return false;
+
+            var validValues = new bool[]
+            {
+                CheckBirth(),
+                CheckIssueYear(),
+                CheckExpiration(),
+                CheckHeight(),
+                CheckHairColor(),
+                CheckEyeColor(),
+                CheckPassportId()
+            };
+            return validValues.All(e => e);
+        }
+
+        private bool CheckBirth()
+        {
+            string s = entries["byr"];
+            int.TryParse(s, out int val);
+            return val >= 1920 && val <= 2002;
+        }
+
+        private bool CheckIssueYear()
+        {
+            string s = entries["iyr"];
+            int.TryParse(s, out int val);
+            return val >= 2010 && val <= 2020;
+        }
+
+        private bool CheckExpiration()
+        {
+            string s = entries["eyr"];
+            int.TryParse(s, out int val);
+            return val >= 2020 && val <= 2030;
+        }
+
+        private bool CheckHeight()
+        {
+            string s = entries["hgt"];
+            if (s.Contains("cm"))
+            {
+                int.TryParse(s.Replace("cm", ""), out int val);
+                return val >= 150 && val <= 193;
+            }
+
+            int.TryParse(s.Replace("in", ""), out int valin);
+            return valin >= 59 && valin <= 76;
+        }
+
+        private bool CheckHairColor()
+        {
+            string s = entries["hcl"];
+            var regex = new Regex("#[a-f0-9]{6}");
+            return regex.IsMatch(s);
+        }
+
+        private bool CheckEyeColor()
+        {
+            string s = entries["ecl"];
+            return s switch
+            {
+                "amb" => true,
+                "blu" => true,
+                "brn" => true,
+                "gry" => true,
+                "grn" => true,
+                "hzl" => true,
+                "oth" => true,
+                _ => false
+            };
+        }
+
+        private bool CheckPassportId()
+        {
+            string s = entries["pid"];
+            return s.Length == 9 && int.TryParse(s, out int _);
+        }
+    }
+}

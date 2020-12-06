f63f42d9636a0f52470ca1903cd09ea1ae1b6d21
OliverHeil <oliver.heil@jentschmann.ch>
2020-12-06 07:01:12 +0100

    Add Day 6 Challenge

---
+++ AdventSolver/Solver/DaySix/CustomDeclerationGroup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventSolver.Solver.DaySix
+{
+    public class CustomDeclarationGroup
+    {
+        private readonly IReadOnlyList<string> currentGroup;
+
+        public CustomDeclarationGroup(IReadOnlyList<string> currentGroup)
+        {
+            this.currentGroup = currentGroup;
+        }
+
+        public int IntersectYesAnswers()
+        {
+            var yesAnswers = currentGroup
+                .Select(val => val.ToCharArray())
+                .ToList();
+
+            var hashSet = new HashSet<char>(yesAnswers.First());
+            foreach (var list in yesAnswers.Skip(1))
+                hashSet.IntersectWith(list);
+
+            return hashSet.Count;
+        }
+
+        public int DistinctYesAnswers()
+        {
+            return string.Concat(currentGroup)
+                .ToCharArray()
+                .Distinct()
+                .Count();
+        }
+    }
+}
---
+++ AdventSolver/Solver/DaySix/DaySixSolver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventSolver.Solver.DaySix
+{
+    [AdventSolverAttribute(6)]
+    public class DaySixSolver : SolverBase, IAdventSolver
+    {
+        public DaySixSolver() : base("Data\\Day6.txt")
+        {
+        }
+
+        private IEnumerable<CustomDeclarationGroup> GetCustomDeclarationQuestionsPerGroup()
+        {
+            var lines = GetDataInput();
+            List<string> currentGroup = new List<string>();
+
+            foreach (string line in lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    yield return new CustomDeclarationGroup(currentGroup);
+                    currentGroup = new List<string>();
+                    continue;
+                }
+                currentGroup.Add(line);
+            }
+
+            yield return new CustomDeclarationGroup(currentGroup);
+        }
+
+        public void Solve()
+        {
+            var questionsPerGroup = GetCustomDeclarationQuestionsPerGroup()
+                .ToList();
+
+            int sumOfAll = questionsPerGroup.Sum(group => group.DistinctYesAnswers());
+            Console.WriteLine($"{sumOfAll} are the sum of all yes answers");
+
+            int intersectYes = questionsPerGroup.Sum(group => group.IntersectYesAnswers());
+            Console.WriteLine($"{intersectYes} are all yes answers in a group answered by everyone");
+        }
+    }
+}

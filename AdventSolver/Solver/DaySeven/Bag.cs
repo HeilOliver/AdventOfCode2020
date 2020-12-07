4b1b4cc332c29c5c29b35bc1e03e1bf1a57ba052
OliverHeil <oliver.heil@jentschmann.ch>
2020-12-07 07:01:36 +0100

    Add Day 7 Challenge

---
+++ AdventSolver/Solver/DaySeven/Bag.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace AdventSolver.Solver.DaySeven
+{
+    public class Bag
+    {
+        public Bag(string bag)
+        {
+            CanCarry = new Dictionary<string, int>();
+
+            var sets = bag.Split(" bags contain ");
+            ColorCode = sets[0];
+
+            string carryInfo = sets[1]
+                .Replace("no other bags.", "")
+                .Replace(" bags.", "")
+                .Replace(" bag.", "")
+                .Replace(" bags", "")
+                .Replace(" bag", "");
+
+            if (string.IsNullOrEmpty(carryInfo.Trim()))
+                return;
+
+            var carryInfoSet = carryInfo
+                .Split(", ");
+
+            foreach (string info in carryInfoSet)
+            {
+                var strings = info.Split(" ");
+                int count = int.Parse(strings[0]);
+                CanCarry.Add($"{strings[1]} {strings[2]}", count);
+            }
+        }
+
+        public string ColorCode { get; }
+
+        public IDictionary<string, int> CanCarry { get; }
+    }
+}
---
+++ AdventSolver/Solver/DaySeven/DaySevenSolver.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventSolver.Solver.DaySix;
+
+namespace AdventSolver.Solver.DaySeven
+{
+    [AdventSolverAttribute(7)]
+    public class DaySevenSolver : SolverBase, IAdventSolver
+    {
+        private const string BagColor = "shiny gold";
+
+        public DaySevenSolver() : base("Data\\Day7.txt")
+        {
+        }
+
+        private IEnumerable<Bag> GetBags()
+        {
+            var lines = GetDataInput();
+         
+            foreach (string line in lines)
+            {
+                yield return new Bag(line);
+            }
+        }
+
+        public void Solve()
+        {
+            var bags = GetBags()
+                .ToHashSet();
+
+            int countOfBags = bags
+                .Select(bag => ContainsColorCode(bag, bags, BagColor))
+                .Count(result => result);
+
+            Console.WriteLine($"{countOfBags} that includes the {BagColor} one");
+
+            var shinyGoldBag = bags
+                .First(e => e.ColorCode.Equals(BagColor));
+            int totalAmount = AmountOfInsertedBags(bags, shinyGoldBag);
+
+            Console.WriteLine($"{totalAmount} Bags are in the {BagColor} bag");
+        }
+
+        private static int AmountOfInsertedBags(HashSet<Bag> bags, Bag bag)
+        {
+            int count = 1;
+            foreach ((string color, int value) in bag.CanCarry)
+            {
+                var bagToFind = bags.First(e => e.ColorCode.Equals(color));
+
+                count += AmountOfInsertedBags(bags, bagToFind) * value;
+            }
+
+            return count;
+        }
+
+        private static bool ContainsColorCode(Bag currentBag, ISet<Bag> bags, string searchedColor, ISet<string> alreadySeen = null)
+        {
+            alreadySeen ??= new HashSet<string>();
+            if (alreadySeen.Contains(currentBag.ColorCode))
+                return false;
+            alreadySeen.Add(currentBag.ColorCode);
+
+            foreach ((string color, int _) in currentBag.CanCarry)
+            {
+                if (color.Equals(searchedColor))
+                    return true;
+
+                var toSearch = bags
+                    .First(e => e.ColorCode.Equals(color));
+
+                bool result = ContainsColorCode(toSearch, bags, searchedColor, alreadySeen);
+
+                if (!result)
+                    continue;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}

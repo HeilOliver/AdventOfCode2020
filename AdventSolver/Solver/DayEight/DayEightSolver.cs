c5fa8f0f5e86021eaa9c65f26fda4b8b21ed3cc3
OliverHeil <oliver.heil@jentschmann.ch>
2020-12-08 15:50:30 +0100

    Put RunResult into extra class file

--- AdventSolver/Solver/DayEight/DayEightSolver.cs
+++ AdventSolver/Solver/DayEight/DayEightSolver.cs
@@ -86,20 +86,4 @@ namespace AdventSolver.Solver.DayEight
             return new RunResult(accumulator, true, false);
         }
     }
-
-    public class RunResult
-    {
-        public RunResult(int value, bool terminates, bool fault)
-        {
-            Value = value;
-            Terminates = terminates;
-            Fault = fault;
-        }
-
-        public int Value { get; }
-
-        public bool Terminates { get; }
-
-        public bool Fault { get; }
-    }
 }
---
+++ AdventSolver/Solver/DayEight/RunResult.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventSolver.Solver.DayEight
+{
+    public class RunResult
+    {
+        public RunResult(int value, bool terminates, bool fault)
+        {
+            Value = value;
+            Terminates = terminates;
+            Fault = fault;
+        }
+
+        public int Value { get; }
+
+        public bool Terminates { get; }
+
+        public bool Fault { get; }
+    }
+}
